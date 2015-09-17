d1b836f98df83eb2e746844caac6dedafbcd9dc8
kmorozov <kmorozov@gmail.com>
2015-09-17 21:05:09 +0300

    packets feature started, some tweaks

--- sweetPrices/src/main/java/ru/morozov/sweetApp/SweetContext.java
+++ sweetPrices/src/main/java/ru/morozov/sweetApp/SweetContext.java
@@ -1,23 +1,19 @@
 package ru.morozov.sweetApp;
 
-import java.util.List;
-
-import javax.annotation.PreDestroy;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import ru.morozov.sweetApp.config.SweetConfig;
 import ru.morozov.sweetApp.config.SweetProduct;
 import ru.morozov.sweetApp.config.SystemConfigs;
 import ru.morozov.sweetApp.config.prices.PricesSet;
-import ru.morozov.sweetApp.config.templates.SweetTemplate;
+
+import javax.annotation.PreDestroy;
+import java.util.List;
 
 public class SweetContext {
 	
 	private static final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring/app-context.xml");
 	
 	public static List<SweetProduct> getProducts() {return ctx.getBean(SweetConfig.class).getSweetsList();}
-	public static SweetTemplate getTemplate(String templateKey) {return (SweetTemplate)ctx.getBean(templateKey);}
 	public static SystemConfigs getSystemConfigs() {return (SystemConfigs)ctx.getBean("systemConfig_demo");}
 	public static PricesSet getPricesSet() {return (PricesSet)ctx.getBean("all_prices");}
 
--- sweetPrices/src/main/java/ru/morozov/sweetApp/config/SweetProduct.java
+++ sweetPrices/src/main/java/ru/morozov/sweetApp/config/SweetProduct.java
@@ -1,21 +1,18 @@
 package ru.morozov.sweetApp.config;
 
-import ru.morozov.sweetApp.SweetContext;
 import ru.morozov.sweetApp.config.templates.SweetTemplate;
 
 public class SweetProduct {
 	
-	private String prefix, description;
+	private String description;
 	private SweetTemplate template;
 
-	public String getPrefix() {return prefix;}
-	public void setPrefix(String prefix) {this.prefix = prefix;}
+	public SweetTemplate getTemplate() {return template;}
+	public void setTemplate(SweetTemplate template) {this.template = template;}
 
 	public String getDescription() {return description;}
 	public void setDescription(String description) {this.description = description;}
 	
 	@Override
 	public String toString() {return description;}
-	
-	public SweetTemplate getTemplate() {return template == null ? template = SweetContext.getTemplate(prefix) : template;}
 }
